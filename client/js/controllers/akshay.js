ce1dc822e5edaf01cdda5a542b412146683ca24a
Akshay <chavanakshay9@gmail.com>
2014-10-30 20:36:34 -0400

    Tagger added on template akshay Issues in Tag Face button

--- client/js/controllers/akshay.js
+++ client/js/controllers/akshay.js
@@ -1,9 +1,69 @@
 Template.akshay.rendered = function(){
-	$('#button').click(Akshay.edgeDetectHorizontal);
+    
+    var canvas = document.getElementById('canvasID'),
+        ctx = canvas.getContext('2d'),
+        rect = {},
+        drag = false,
+        rectDisplay = "";
+    
+    function init() {
+      canvas.addEventListener('mousedown', mouseDown, false);
+      canvas.addEventListener('mouseup', mouseUp, false);
+      canvas.addEventListener('mousemove', mouseMove, false);
+    }
+
+    function mouseDown(e) {
+      rect.startX = e.pageX - this.offsetLeft;
+      rect.startY = e.pageY - this.offsetTop;
+      drag = true;
+    }
+        
+    function mouseUp() {
+      drag = false;
+    }
+
+    function mouseMove(e) {
+      if (drag) {
+        rect.w = (e.pageX - this.offsetLeft) - rect.startX;
+        rect.h = (e.pageY - this.offsetTop) - rect.startY ;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        draw();
+      }
+    }
+
+    function draw() {
+      ctx.lineWidth   = 2;
+        ctx.lineJoin    = 'round';
+      ctx.strokeStyle = "#00FF00";
+        ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+
+        var rectDisplay = "Rect : ";
+        if (rect.w < 0)
+          rectDisplay += (rect.startX + rect.w).toString() + " , ";
+        else
+          rectDisplay += rect.startX.toString() + " , ";
+
+        if (rect.h < 0)
+          rectDisplay += (rect.startY + rect.h).toString() + " , ";
+        else 
+          rectDisplay += rect.startY.toString() + " , ";
+
+        rectDisplay += Math.abs(rect.w).toString() + " , ";
+        rectDisplay += Math.abs(rect.h).toString();
+
+      document.getElementById("rectco").innerHTML = rectDisplay;
+    }
+
+    $('#tagID').click( function(){
+      console.log(rectDisplay);
+    });
+
+    init();
 }
 
 Akshay = {
-  logClicked: function(what) {
+
+  /*logClicked: function(what) {
   	console.log('clicked: ');
   	console.log(what);
   },
@@ -43,5 +103,5 @@ Akshay = {
     }
     canvasContext.putImageData(imageData, 0, 0);
 
-  }
+  }*/
 };
